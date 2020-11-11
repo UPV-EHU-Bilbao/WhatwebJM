de8399ab6225b2ac5311ceb7bbbd982ed51018e5
jonander <jonanderasua@gmail.com>
2020-11-11 16:17:25 +0100

    fontawesome kudeaketa

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+open module whatweb {
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires de.jensd.fx.fontawesomefx.fontawesome;
+
+    exports whatweb;
+}
